22e9f91fed7585326fe72ee1f9da28c66ef7baf2
Pierre <monvoisin.pierre@gmail.com>
2019-12-06 10:13:29 +0100

    Partie 2 terminée

---
+++ Exercice3/index.php
@@ -0,0 +1,46 @@
+<?php $gender = 'Homme'; $age = 21; ?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <title>Exercice 3</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body class='container-fluid bg-secondary'>
+
+  <div class="row mt-5">
+    <div class="jumbotron shadow-lg mx-auto text-center">
+      <p>
+        <?php
+        if($gender == 'Homme'){
+          if($age > 0 && $age < 18){
+            echo 'Vous êtes un homme et vous êtes mineur';
+          } else if($age >= 18 && $age < 122){
+            echo 'Vous êtes un homme et vous êtes majeur';
+          } else {
+            echo 'Vous êtes un homme qui a mal entré son âge';
+          }
+        } else if($gender == 'Femme'){
+          if($age > 0 && $age < 18){
+            echo 'Vous êtes une femme et vous êtes mineur';
+          } else if($age >= 18 && $age < 122){
+            echo 'Vous êtes une femme et vous êtes majeur';
+          } else {
+            echo 'Vous êtes une femme qui a mal entré son âge';
+          }
+        } else {
+          echo 'Je ne reconnais pas votre genre';
+        }
+        ?>
+      </p>
+      <div class="text-right mt-1">Un message de la direction</div>
+    </div>
+  </div>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+</body>
+</html>
